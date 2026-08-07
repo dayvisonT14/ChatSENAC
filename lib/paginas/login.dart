70bef0078840f7bbb8f46958792771574a85aef4
Dayvison <dayvisontv56@gmail.com>
2026-08-06 21:34:14 -0300

    fizemos a função de fazer login

--- lib/paginas/login.dart
+++ lib/paginas/login.dart
@@ -1,26 +1,53 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:primeiro_app/paginas/DashBoard.dart';
+import 'package:http/http.dart' as http;
 import 'package:primeiro_app/paginas/cadastro.dart';
+import 'package:primeiro_app/paginas/dashboard.dart';
 import 'package:primeiro_app/utilitarios/tipografia.dart';
 
 class Login extends StatefulWidget {
+  const Login({super.key});
+
   @override
-  State<StatefulWidget> createState() => _LoginState();
+  State<Login> createState() => _LoginState();
 }
 
 class _LoginState extends State<Login> {
   final emailControlador = TextEditingController();
   final senhaControlador = TextEditingController();
 
-  void fazerLogin() {
-    if (emailControlador.text != "Test@gmail.com" ||
-        senhaControlador.text != "123456") {
+  Future fazerLogin() async {
+    var url = Uri.http("10.112.4.33", "login");
+
+    var resposta = await http.post(
+      url,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: jsonEncode({
+        "email": emailControlador.text,
+        "senha": senhaControlador.text,
+      }),
+    );
+
+    if (resposta.statusCode != 200) {
+      var dados = jsonDecode(resposta.body);
+
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Email e/ou senha estão incorretas")),
+        SnackBar(
+          content: Text("${dados["message"]}"),
+        ),
       );
+
       return;
     }
-    Navigator.push(context, MaterialPageRoute(builder: (build) => DashBoard()));
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => DashBoard(),
+      ),
+    );
   }
 
   @override
@@ -28,134 +55,172 @@ class _LoginState extends State<Login> {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 32),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Row(children: [FlutterLogo(size: 18), Text("ChatSENAC")]),
-                SizedBox(height: 32),
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Row(
+                children: const [
+                  FlutterLogo(size: 18),
+                  SizedBox(width: 8),
+                  Text("ChatSENAC"),
+                ],
+              ),
 
-                // Titulos
-                Text("Entre na sua Conta", style: Tipografia.h1),
-                SizedBox(height: 12),
-                Text(
-                  "Coloque o seu email e senha para logar",
-                  style: Tipografia.subtitulo,
-                ),
-                SizedBox(height: 32),
+              const SizedBox(height: 32),
 
-                // Campos
-                Text("Email", style: Tipografia.subtitulo),
-                TextField(
-                  controller: emailControlador,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+              Text("Entre na sua conta", style: Tipografia.h1),
+
+              const SizedBox(height: 12),
+
+              Text(
+                "Coloque o seu email e senha para logar",
+                style: Tipografia.subtitulo,
+              ),
+
+              const SizedBox(height: 32),
+
+              const Text("Email"),
+
+              const SizedBox(height: 4),
+
+              TextField(
+                controller: emailControlador,
+                decoration: InputDecoration(
+                  hintText: "exemplo@gmail.com",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
                   ),
                 ),
-                SizedBox(height: 16),
-                Text("Senha", style: Tipografia.subtitulo),
-                TextField(
-                  controller: senhaControlador,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    suffixIcon: Icon(Icons.visibility_off),
+              ),
+
+              const SizedBox(height: 16),
+
+              const Text("Senha"),
+
+              const SizedBox(height: 4),
+
+              TextField(
+                controller: senhaControlador,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "••••••••",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  obscureText: true,
-                ),
-                SizedBox(height: 16),
-                InkWell(
-                  onTap: () {},
-                  child: Text(
-                    "Esqueceu a senha?",
-                    textAlign: TextAlign.right,
-                    style: Tipografia.link,
+                  suffixIcon: const Icon(Icons.visibility_off),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 16,
+                    vertical: 12,
                   ),
                 ),
-                SizedBox(height: 24),
-                // Botões
-                ElevatedButton(
+              ),
+
+              const SizedBox(height: 12),
+
+              InkWell(
+                onTap: () {},
+                child: Text(
+                  "Esqueceu a senha?",
+                  style: Tipografia.link,
+                  textAlign: TextAlign.right,
+                ),
+              ),
+
+              const SizedBox(height: 24),
+
+              SizedBox(
+                height: 48,
+                child: ElevatedButton(
                   onPressed: fazerLogin,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.blue,
                     foregroundColor: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                    elevation: 0,
+                    shape: const RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10)),
                     ),
-                    padding: EdgeInsets.symmetric(vertical: 16),
                   ),
-                  child: Text("Entrar", style: Tipografia.subtitulo),
+                  child: const Text(
+                    "Entrar",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
                 ),
-                SizedBox(height: 24),
-                Text("Ou", textAlign: TextAlign.center),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black87,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+              ),
+
+              const SizedBox(height: 16),
+
+              const Text(
+                "ou",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.grey),
+              ),
+
+              const SizedBox(height: 16),
+
+              SizedBox(
+                height: 48,
+                child: OutlinedButton(
                   onPressed: () {},
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 10,
                     children: [
                       Image.asset("assets/imagens/google-icon.png", height: 18),
-                      Text("Continuar com Google", style: Tipografia.subtitulo),
-                    ],
-                  ),
-                ),
-                SizedBox(height: 16),
-
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black87,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
-                  onPressed: () {},
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    spacing: 10,
-                    children: [
-                      Image.asset(
-                        "assets/imagens/facebook-icon.png",
-                        height: 18,
-                      ),
+                      const SizedBox(width: 10),
                       Text(
-                        "Continuar com Facebook",
+                        "Continuar com o Google",
                         style: Tipografia.subtitulo,
                       ),
                     ],
                   ),
                 ),
-                SizedBox(height: 54),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  spacing: 6,
-                  children: [
-                    Text("Não tem uma conta?", style: Tipografia.subtitulo),
-                    InkWell(
-                      child: Text("Cadastre-se", style: Tipografia.link),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (builder) => Cadastro()),
-                        );
-                      },
-                    ),
-                  ],
+              ),
+
+              const SizedBox(height: 12),
+
+              SizedBox(
+                height: 48,
+                child: OutlinedButton(
+                  onPressed: () {},
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        "assets/imagens/facebook-icon.png",
+                        height: 18,
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        "Continuar com o Facebook",
+                        style: Tipografia.subtitulo,
+                      ),
+                    ],
+                  ),
                 ),
-              ],
-            ),
+              ),
+
+              const SizedBox(height: 54),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Não tem uma conta? "),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (_) => Cadastro()),
+                      );
+                    },
+                    child: Text("Cadastre-se", style: Tipografia.link),
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
